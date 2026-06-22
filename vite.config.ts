ac95572868e64037426b16227066102d756d3a2e
sorakand <sorakagracefully@gmail.com>
2026-06-23 03:51:50 +0800

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {fileURLToPath, URL} from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/web',
+    base: '/',
     plugins: [vue(), tailwindcss()],
     build: {
         emptyOutDir: true,
